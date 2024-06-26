7d3c6bb27a678529b013141474eb9511848416ee
Abhishek Singh <abhi209304@gmail.com>
2024-06-26 09:26:40 +0530

    Update UserData.js

--- src/data/UserData.js
+++ src/data/UserData.js
@@ -1,17 +1,16 @@
 const UserData = [
-    { userId: 1, fullName: 'John Doe', email: 'john.doe@example.com' },
-    { userId: 2, fullName: 'Jane Smith', email: 'jane.smith@example.com' },
-    { userId: 3, fullName: 'Michael Johnson', email: 'michael.johnson@example.com' },
-    { userId: 4, fullName: 'Emily Brown', email: 'emily.brown@example.com' },
-    { userId: 5, fullName: 'William Wilson', email: 'william.wilson@example.com' },
-    { userId: 6, fullName: 'Sophia Lee', email: 'sophia.lee@example.com' },
-    { userId: 7, fullName: 'James Davis', email: 'james.davis@example.com' },
-    { userId: 8, fullName: 'Olivia Martinez', email: 'olivia.martinez@example.com' },
-    { userId: 9, fullName: 'Alexander Rodriguez', email: 'alexander.rodriguez@example.com' },
-    { userId: 10, fullName: 'Isabella Lopez', email: 'isabella.lopez@example.com' },
-    { userId: 11, fullName: 'Ethan Hernandez', email: 'ethan.hernandez@example.com' },
-    { userId: 12, fullName: 'Mia Gonzalez', email: 'mia.gonzalez@example.com' },
+    { userId: 1, fullName: 'Vinod Singh', email: 'vinod@example.com' },
+    { userId: 2, fullName: 'Vaibhav Tiwari', email: 'tiwari.vaibhav@example.com' },
+    { userId: 3, fullName: 'Pratyush Srivastava', email: 'Pratyy@example.com' },
+    { userId: 4, fullName: 'Harsh Singh', email: 'Harsh_asingh@example.com' },
+    { userId: 5, fullName: 'Harsh Kumar', email: 'HarshK@example.com' },
+    { userId: 6, fullName: 'Harsh Maurya', email: 'Maurya.Harsh@example.com' },
+    { userId: 7, fullName: 'Abhishek Singh', email: 'Abhishek.singh@example.com' },
+    { userId: 8, fullName: 'Santosh Singh', email: 'SinghSantosh@example.com' },
+    { userId: 9, fullName: 'Devendra Gupta', email: 'Devendrag@example.com' },
+    { userId: 10, fullName: 'Shobhit Dwivedi', email: 'shobhit@example.com' },
+    { userId: 11, fullName: 'Harshit Singh', email: 'harshit.singh@example.com' },
+    { userId: 12, fullName: 'Marry Jane', email: 'jane.marry@example.com' },
   ];
   
   export default UserData;
-  
